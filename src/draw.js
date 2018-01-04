210ac25272330340a54774b18ca5334940b9d81d
Robert Smith <ert135@gmail.com>
2018-01-04 19:14:25 +0000

    Please enter the commit message for your changes. Lines starting

--- src/draw.js
+++ src/draw.js
@@ -1,32 +1,38 @@
 let size = 800;
 let angle = 0.01;
+let barWidth = 50;
 
 let setup = function() {
     createCanvas(size, size, WEBGL);
 }
 
 let draw = function() {
-    background(175);
-    pointLight(255, 0, 0, -200, 0, 0);
-    pointLight(0, 0, 255, 0, 200, 0);
+    background(176);
+    // ortho()
 
-    push();
-    rotateX(angle);
-    rotateY(angle * 0.3);
-    rotateZ(angle);
+    translate(0, 50, -50);
+    rotateX(PI / 8);
 
-    ambientMaterial(255);
-    noStroke();
-    box(200)
-    pop()
+    rectMode(CENTER);
 
-    translate(0, 300);
-    rotateX(HALF_PI);
+    let offset = 0;
+    for (let x = 0; x < width; x += barWidth) {
+        push();
+            let a = angle + offset;
+            let h = map(sin(a), -1, 1, 0, 100);
 
-    ambientMaterial(255);
-    plane(600, 600);
+            fill(255);
+            stroke(0);
+            translate(x - width / 2 + barWidth/2, 0, 0);
 
-    angle += 0.003;
+            normalMaterial();
+            box(barWidth, h, barWidth);
+
+            offset += 0.1;
+        pop();
+    }
+
+    angle += 0.1;
 }
 
 window.setup = setup;
