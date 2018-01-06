8fa35b574987601b7275f8c7c8bfbc32ee9e7309
Robert Smith <ert135@gmail.com>
2018-01-06 14:12:09 +0000

    added offset from center

--- src/draw.js
+++ src/draw.js
@@ -1,38 +1,38 @@
 let size = 800;
 let angle = 0.01;
-let barWidth = 50;
+let barWidth = 20;
+let isometricAngle;
+let maxD;
+let w = 20;
 
 let setup = function() {
     createCanvas(size, size, WEBGL);
+    isometricAngle = atan(1 / sqrt(2));
+    maxD = dist(0, 0, 200, 200);
 }
 
 let draw = function() {
-    background(176);
-    // ortho()
+    background(255);
+    ortho(-500, 500, 500, -500, 0, 1200);
 
-    translate(0, 50, -50);
-    rotateX(PI / 8);
+    rotateX(-isometricAngle);
+    rotateY(-QUARTER_PI);
 
-    rectMode(CENTER);
-
-    let offset = 0;
-    for (let x = 0; x < width; x += barWidth) {
+    for (let z = 0; z < height; z += w) {
+      for (let x = 0; x < width; x += w) {
         push();
+            let distanceFromCenter = dist(x, z, width / 2, height / 2);
+            let offset = map(distanceFromCenter, 0, maxD, -1, 1)
             let a = angle + offset;
-            let h = map(sin(a), -1, 1, 0, 100);
-
-            fill(255);
-            stroke(0);
-            translate(x - width / 2 + barWidth/2, 0, 0);
-
+            let h = floor(map(sin(a), -1, 1, 100, 300));
+            translate(x - width / 2, 0, z - height / 2);
             normalMaterial();
-            box(barWidth, h, barWidth);
-
-            offset += 0.1;
+            box(w - 2, h, w - 2);
         pop();
+      }
     }
-
-    angle += 0.1;
+  
+    angle -= 0.1;
 }
 
 window.setup = setup;
